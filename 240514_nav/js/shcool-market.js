a6e4c15cc250ce7392e2c4cdde592574336d229b
김가현 <rkgus0324@gmail.com>
2024-06-18 15:54:32 +0900

    name과 category 둘다 검색 완✌️

--- 240514_nav/js/shcool-market.js
+++ 240514_nav/js/shcool-market.js
@@ -40,7 +40,7 @@ getData();
 const searchData = (query) => {
     // console.log(query)
     // data 하나씩 꺼내어, name이랑 query랑 비교해서 있으면, 모아놓자
-    let searchData = allData.filter((oneData) => oneData["name"].includes(query));
+    let searchData = allData.filter((oneData) => oneData["name"].includes(query) || oneData["category"].includes(query));
     showData(searchData);
 
 }
