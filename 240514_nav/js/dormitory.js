91c356b72f1be766923896a909ab6fd6f40b8f3a
김가현 <rkgus0324@gmail.com>
2024-10-25 12:02:31 +0900

    dormitory selection 선택, 각 페이지 보이기 html, css, js

---
+++ 240514_nav/js/dormitory.js
@@ -0,0 +1,38 @@
+// selection-item 요소들 가져오자
+const selectionItemDivs = document.getElementsByClassName("selection-item");
+
+// 네 개의 페이지 요소 가져오자.
+const calendarDiv = document.getElementById("calendar");
+const selectionWashingmachineTimeDiv = document.getElementById("selection-washingmachine-time");
+const selectionRoomNameDiv = document.querySelector("#selection-room-name");
+const boardDiv = document.querySelector("#board");
+
+// console.log(calendarDiv);
+// console.log(selectionWashingmachineTimeDiv);
+// console.log(selectionRoomNameDiv);
+// console.log(boardDiv);
+
+const pageDivs = [calendarDiv, selectionWashingmachineTimeDiv, selectionRoomNameDiv, boardDiv];
+console.log(pageDivs);
+
+// 무조건 1페이지가 아님.
+const setPage = (page) => {
+    // clear selelction
+    for (const selectionItemDiv of selectionItemDivs) {
+        selectionItemDiv.classList.remove("select-menu");
+    }
+
+    // selection 치하자
+    selectionItemDivs[page - 1].classList.add("select-menu");
+
+    // clear pageDiv
+    pageDivs.forEach(pageDiv => {
+        pageDiv.style.display = "none";
+    });
+
+    // show pageDiv 1
+    pageDivs[page - 1].style.display = "block";
+}
+setPage(1)
+
+
